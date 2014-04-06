ca8e5389d471363cd767286db2291e4bc81c3a70
Daniel Feichtinger <daniel@auxilit.com>
2014-04-06 13:31:02 +0800

    Transfer request must be signed.

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -3,10 +3,22 @@ class TransfersController < ApplicationController
   respond_to :json
   
   def create
-    source = Account.find_by_code(params[:source])
-    destination = Account.find_by_code(params[:destination])
-    transfer = Transfer.create(source: source, destination: destination, amount: params[:amount])
+    source = Account.find_by_code(transfer_params[:source])
+    digest = OpenSSL::Digest::SHA256.new
+    key = OpenSSL::PKey::RSA.new(source.public_key)
+    raise unless key.verify(digest, Base64.decode64(params[:signature]), transfer_params.to_json)
+
+    destination = Account.find_by_code(transfer_params[:destination])
+    transfer = Transfer.create(source: source, destination: destination, amount: transfer_params[:amount])
     respond_with transfer
+  rescue
+    head :unprocessable_entity
+  end
+  
+private
+  
+  def transfer_params
+    params.require(:transfer).permit(:source, :destination, :amount)
   end
   
 end
--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -3,25 +3,60 @@ require 'test_helper'
 class TransfersControllerTest < ActionController::TestCase
   
   setup do
-    c = Currency.create!(public_key: '123', name: 'Moonbucks', url: 'http://moonbucks.com')
-    Issue.create!(currency_id: c.id, amount: 2000)
+    @k1 = OpenSSL::PKey::RSA.new(2048)
+    @k2 = OpenSSL::PKey::RSA.new(2048)
+    @pk1 = @k1.public_key.to_pem
+    @pk2 = @k2.public_key.to_pem
+    @digest = OpenSSL::Digest::SHA256.new
+
+    c = Currency.create!(public_key: @pk1, name: 'Moonbucks', url: 'http://moonbucks.com')
+    Issue.create!(currency: c, amount: 2000)
     @s = c.primary_account
-    @d = Account.create!(public_key: '456', currency_id: c.id)
+    @d = Account.create!(public_key: @pk2, currency: c)
   end
   
   test "valid POST should be successful" do
-    post :create, source: @s.code, destination: @d.code, amount: 500, format: :json
+    valid_post
     assert_equal '201', response.code
   end
   
   test "valid POST should decrease source account balance" do
-    post :create, source: @s.code, destination: @d.code, amount: 500, format: :json
+    valid_post
     assert_equal 1500, @s.reload.balance
   end
   
   test "valid POST should increase destination account balance" do
-    post :create, source: @s.code, destination: @d.code, amount: 500, format: :json
+    valid_post
     assert_equal 500, @d.reload.balance
+  end
+  
+  test "invalid POST should be unsuccessful" do
+    invalid_post
+    assert_equal '422', response.code
+  end
+  
+  test "invalid POST should not change source account balance" do
+    invalid_post
+    assert_equal 2000, @s.reload.balance
+  end
+  
+  test "invalid POST should not change destination account balance" do
+    invalid_post
+    assert_equal 0, @d.reload.balance
+  end
+  
+private
+  
+  def valid_post
+    data = { source: @s.code, destination: @d.code, amount: 500 }
+    sig  = Base64.encode64 @k1.sign(@digest, data.to_json)
+    post :create, transfer: data, signature: sig, format: :json
+  end
+  
+  def invalid_post
+    data = { source: @s.code, destination: @d.code, amount: 500 }
+    sig  = Base64.encode64 @k2.sign(@digest, data.to_json)
+    post :create, transfer: data, signature: sig, format: :json
   end
   
 end
