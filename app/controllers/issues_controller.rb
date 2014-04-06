147ead539c3d97a89641ce58213b546896db8506
Daniel Feichtinger <daniel@auxilit.com>
2014-04-06 19:00:02 +0800

    Require signature on issue requests.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,9 +3,21 @@ class IssuesController < ApplicationController
   respond_to :json
   
   def create
-    currency = Currency.find_by_name(params[:currency])
-    issue = Issue.create(amount: params[:amount], currency: currency)
+    currency = Currency.find_by_name(issue_params[:currency])
+    digest = OpenSSL::Digest::SHA256.new
+    key = OpenSSL::PKey::RSA.new(currency.public_key)
+    raise unless key.verify(digest, Base64.decode64(params[:signature]), issue_params.to_json)
+    
+    issue = Issue.create(currency: currency, amount: issue_params[:amount])
     respond_with issue
+  rescue
+    head :unprocessable_entity
+  end
+  
+private
+  
+  def issue_params
+    params.require(:issue).permit(:currency, :amount)
   end
   
 end
--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -3,17 +3,44 @@ require 'test_helper'
 class IssuesControllerTest < ActionController::TestCase
   
   setup do
-    Currency.create!(public_key: '123', name: 'Moonbucks', url: 'http://moonbucks.com')
+    @key = OpenSSL::PKey::RSA.new(2048)
+    @public_key = @key.public_key.to_pem
+    Currency.create!(public_key: @public_key, name: 'Moonbucks', url: 'http://moonbucks.com')
   end
   
   test "valid POST should be successful" do
-    post :create, currency: 'Moonbucks', amount: 1000, format: :json
+    valid_post
     assert_equal '201', response.code
   end
   
-  test "valid POST should increase primary wallet balance" do
-    post :create, currency: 'Moonbucks', amount: 1000, format: :json
+  test "valid POST should increase primary account balance" do
+    valid_post
     assert_equal 1000, Account.first.balance
+  end
+  
+  test "invalid POST should be unsuccessful" do
+    invalid_post
+    assert_equal '422', response.code
+  end
+  
+  test "invalid POST should not change primary account balance" do
+    invalid_post
+    assert_equal 0, Account.first.balance
+  end
+  
+private
+  
+  def valid_post
+    data = { currency: 'Moonbucks', amount: 1000 }
+    sig  = Base64.encode64 @key.sign(OpenSSL::Digest::SHA256.new, data.to_json)
+    post :create, issue: data, signature: sig, format: :json
+  end
+  
+  def invalid_post
+    bad_key = OpenSSL::PKey::RSA.new(2048)
+    data = { currency: 'Moonbucks', amount: 1000 }
+    sig  = Base64.encode64 bad_key.sign(OpenSSL::Digest::SHA256.new, data.to_json)
+    post :create, issue: data, signature: sig, format: :json
   end
   
 end
