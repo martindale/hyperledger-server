3d4d85b7e124f7740fb0ea5c18186a4ce2d643dd
Daniel Feichtinger <daniel@auxilit.com>
2014-04-14 19:02:07 +0800

    Move to Postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140325022654) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: true do |t|
     t.string   "code"
     t.text     "public_key"
