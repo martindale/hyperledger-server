47ab281e0f115490a50f91ec6860ebec31bc5652
Daniel Feichtinger <daniel@auxilit.com>
2014-05-28 17:06:53 +0800

    Consensus pool verifies confirmations.

--- app/models/consensus_pool.rb
+++ app/models/consensus_pool.rb
@@ -10,6 +10,11 @@ class ConsensusPool
     @quorum = @servers.count - max_failures
   end
   
-  def valid_confirmation?(confirmation, data)
+  def valid_confirmation?(server_id, signature, data)
+    confirmation_server = servers.find { |s| s[:url] == server_id }
+    return false unless confirmation_server
+    
+    key = OpenSSL::PKey::RSA.new(confirmation_server[:public_key])
+    key.verify(OpenSSL::Digest::SHA256.new, Base64.decode64(signature), data.to_json)
   end
 end
--- test/models/consensus_pool_test.rb
+++ test/models/consensus_pool_test.rb
@@ -2,8 +2,28 @@ require 'test_helper'
 
 class ConsensusPoolTest < ActiveSupport::TestCase
   
+  setup do
+    @key = OpenSSL::PKey::RSA.new(2048)
+    @mock_server = { url: 'test', public_key: @key.public_key.to_pem }
+  end
+  
   test 'the consensus pool is initialised' do
     assert_equal 'Test', ConsensusPool.instance.name
+  end
+  
+  test '#valid_confirmation? returns true for valid signature' do
+    sign = Base64.encode64 @key.sign(OpenSSL::Digest::SHA256.new, {test: 'test'}.to_json)
+    ConsensusPool.instance.stub :servers, [@mock_server] do
+      assert ConsensusPool.instance.valid_confirmation?('test', sign, {test: 'test'})
+    end
+  end
+  
+  test '#valid_confirmation? returns false for invalid signature' do
+    key2 = OpenSSL::PKey::RSA.new(2048)
+    bad_sign = Base64.encode64 key2.sign(OpenSSL::Digest::SHA256.new, {test: 'test'}.to_json)
+    ConsensusPool.instance.stub :servers, [@mock_server] do
+      refute ConsensusPool.instance.valid_confirmation?('test', bad_sign, {test: 'test'})
+    end
   end
   
 end
