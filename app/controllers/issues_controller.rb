79124e9092794ad2a2a1c0ca5d35dbc279514948
Daniel Feichtinger <daniel@auxilit.com>
2014-06-23 18:01:36 +0800

    Quick implementation of issue confirmation for testing.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -8,8 +8,18 @@ class IssuesController < ApplicationController
     key = OpenSSL::PKey::RSA.new(ledger.public_key)
     raise unless key.verify(digest, Base64.decode64(params[:signature]), issue_params.to_json)
     
-    issue = Issue.create(ledger: ledger, amount: issue_params[:amount])
-    respond_with issue
+    existing_issue = Issue.where(issue_params).first
+    if confirmed?(combined_params) && existing_issue
+      existing_issue.add_confirmation
+      head :no_content
+    elsif !existing_issue
+      issue = Issue.create(ledger: ledger, amount: issue_params[:amount])
+      ConsensusPool.instance.broadcast(:issue, combined_params) if issue.valid?
+      respond_with issue
+    else
+      raise
+    end
+    
   rescue
     head :unprocessable_entity
   end
@@ -18,6 +28,10 @@ private
   
   def issue_params
     params.require(:issue).permit(:ledger, :amount)
+  end
+  
+  def combined_params
+    { issue: issue_params, signature: params[:signature] }
   end
   
 end
