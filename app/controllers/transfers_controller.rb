5376dc4ba7e4004e061f20506f02f921cd89c7c4
Daniel Feichtinger <daniel@auxilit.com>
2014-06-24 10:30:28 +0800

    Basic transfer replication.

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -7,10 +7,20 @@ class TransfersController < ApplicationController
     digest = OpenSSL::Digest::SHA256.new
     key = OpenSSL::PKey::RSA.new(source.public_key)
     raise unless key.verify(digest, Base64.decode64(params[:signature]), transfer_params.to_json)
-
-    destination = Account.find_by_code(transfer_params[:destination])
-    transfer = Transfer.create(source: source, destination: destination, amount: transfer_params[:amount])
-    respond_with transfer
+    
+    existing_transfer = Transfer.where(transfer_params).first
+    if confirmed?(transfer_params) && existing_transfer
+      existing_transfer.add_confirmation
+      head :no_content
+    elsif !existing_transfer
+      destination = Account.find_by_code(transfer_params[:destination])
+      transfer = Transfer.create(source: source, destination: destination, amount: transfer_params[:amount])
+      ConsensusPool.instance.broadcast(:transfer, transfer_params) if transfer.valid?
+      respond_with transfer
+    else
+      raise
+    end
+    
   rescue
     head :unprocessable_entity
   end
