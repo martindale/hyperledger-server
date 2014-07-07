dab2ae7c25952ad1ae4b4b39a107b16cbcd4c52a
Daniel Feichtinger <daniel@auxilit.com>
2014-07-07 21:44:10 +0800

    Include prepare and commits in ledger and account serialisers.

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,5 +1,7 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :code, :balance, :ledger, :public_key
+  attributes :code, :balance, :ledger, :public_key, :prepared, :committed
+  has_many :prepare_confirmations
+  has_many :commit_confirmations
   
   def ledger
     object.ledger.name
--- app/serializers/ledger_serializer.rb
+++ app/serializers/ledger_serializer.rb
@@ -1,4 +1,6 @@
 class LedgerSerializer < ActiveModel::Serializer
-  attributes :name, :public_key, :url
+  attributes :name, :public_key, :url, :prepared, :committed
   has_one :primary_account
+  has_many :prepare_confirmations
+  has_many :commit_confirmations
 end
